b046750aedc797ec6e4f49b438c84b7e01489e8b
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-03-13 10:53:35 +0530

    peak index question

---
+++ Searching/Binary Search/Questions/PeakIndex_MountainArr.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Time Complexity -> O(log n)
+int peakIndex(vector<int> arr)
+{
+    int start = 0;
+    int end = arr.size() - 1;
+    int mid = start + (end - start) / 2;
+
+    while (start < end)
+    {
+        // search right
+        if (arr[mid] < arr[mid + 1])
+            start = mid + 1;
+        else
+            end = mid;
+
+        mid = start + (end - start) / 2;
+    }
+
+    return start;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    // taking input in vector
+    vector<int> arr(n);
+    cout << "Enter the elements of array: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int result = peakIndex(arr);
+    cout << "The peak element is at index " << result;
+
+    return 0;
+}

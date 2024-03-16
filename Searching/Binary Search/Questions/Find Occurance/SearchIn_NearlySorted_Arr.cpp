3fdfeedb88d507b3d2c990a2733665c44a81c981
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-03-16 12:50:30 +0530

    just a comment

--- Searching/Binary Search/Questions/Find Occurance/SearchIn_NearlySorted_Arr.cpp
+++ Searching/Binary Search/Questions/Find Occurance/SearchIn_NearlySorted_Arr.cpp
@@ -7,7 +7,7 @@ int main()
     int n;
     cout << "Enter the number of elements: ";
     cin >> n;
-
+    // taking input in vector
     vector<int> arr(n);
     cout << "Enter the elements of array: ";
     for (int i = 0; i < n; i++)

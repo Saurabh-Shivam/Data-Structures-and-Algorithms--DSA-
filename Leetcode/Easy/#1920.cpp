d096d0f1e60351e9bc7bb80f53877c8f8a80bfbd
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-05-02 15:48:24 +0530

    solved problem number 1512, 1920 and 75

--- Leetcode/Easy/#1920.cpp
+++ Leetcode/Easy/#1920.cpp
@@ -2,6 +2,18 @@
 #include <vector>
 using namespace std;
 
+// TODO: Optimise the code to O(1) Space Complexity
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+vector<int> buildArray(vector<int> &nums)
+{
+    vector<int> ans;
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+        ans.push_back(nums[nums[i]]);
+
+    return ans;
+}
 
 int main()
 {
--- Leetcode/Medium/#75.cpp
+++ Leetcode/Medium/#75.cpp
@@ -4,7 +4,31 @@ using namespace std;
 
 void sortColors(vector<int> &nums)
 {
-    
+    // DUTCH NATIONAL FLAG ALGORITHM QUESTION
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    int i = 0;
+    int start = 0;
+    int end = nums.size() - 1;
+    while (i <= end)
+    {
+        if (nums[i] == 0)
+        {
+            swap(nums[i], nums[start]);
+            start++;
+            i++;
+        }
+        else if (nums[i] == 2)
+        {
+            swap(nums[i], nums[end]);
+            end--;
+        }
+        // if nums[i] == 1
+        else
+        {
+            i++;
+        }
+    }
 }
 
 int main()

24cccdc5051650f45cb8cb5a0c104868c45035ae
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-05-14 21:35:36 +0530

    just a comment

--- Searching/Binary Search/ExponentialSearch.cpp
+++ Searching/Binary Search/ExponentialSearch.cpp
@@ -35,7 +35,6 @@ int main()
     cout << "Enter the number of elements: ";
     cin >> n;
 
-    // taking input in array
     int arr[500];
     cout << "Enter the elements of array:" << endl;
     for (int i = 0; i < n; i++)
